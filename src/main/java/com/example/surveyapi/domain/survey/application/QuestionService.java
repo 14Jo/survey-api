98cdd24acbac7f0a82b9e25bbe29e8c711a86fd5
이준영 <tera199810@gmail.com>
2025-07-22 16:11:16 +0900

    feat : 질문 생성 시 이벤트 발행
    질문 생성 시 관련 선택지 이벤트 발행
    해당 이벤트의 리스너 구현

--- src/main/java/com/example/surveyapi/domain/survey/application/QuestionService.java
+++ src/main/java/com/example/surveyapi/domain/survey/application/QuestionService.java
@@ -2,11 +2,13 @@ package com.example.surveyapi.domain.survey.application;
 
 import java.util.List;
 
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
 
 import com.example.surveyapi.domain.survey.domain.question.Question;
 import com.example.surveyapi.domain.survey.domain.question.QuestionRepository;
-import com.example.surveyapi.domain.survey.domain.request.CreateQuestionRequest;
+import com.example.surveyapi.domain.survey.application.request.CreateQuestionRequest;
+import com.example.surveyapi.domain.survey.domain.question.event.QuestionCreateEvent;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -17,19 +19,23 @@ import lombok.extern.slf4j.Slf4j;
 public class QuestionService {
 
 	private final QuestionRepository questionRepository;
+	private final ApplicationEventPublisher eventPublisher;
 
+	//TODO 트랜잭션 관리 방법 생각하기
+	//TODO 벌크 인서트 고려하기
 	public void create(Long surveyId, List<CreateQuestionRequest> questions) {
 		long startTime = System.currentTimeMillis();
-		List<Question> questionList = questions.stream()
-			.map(question -> Question.create(
+		questions.forEach(question -> {
+			Question q = Question.create(
 				surveyId,
 				question.getContent(),
 				question.getQuestionType(),
 				question.getDisplayOrder(),
 				question.isRequired()
-			)).toList();
-
-		questionRepository.saveAll(questionList);
+			);
+			Question save = questionRepository.save(q);
+			eventPublisher.publishEvent(new QuestionCreateEvent(save.getQuestionId(), question.getChoices()));
+		});
 		long endTime = System.currentTimeMillis();
 		log.info("질문 생성 시간 - 총 {} ms", endTime - startTime);
 	}
