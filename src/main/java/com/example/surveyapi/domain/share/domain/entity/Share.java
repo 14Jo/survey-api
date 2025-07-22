cc8589757a59a728ecdc09323366787b309272cf
easter1201 <kite1201kr@gmail.com>
2025-07-22 12:26:46 +0900

    feat : Share 기본 생성

---
+++ src/main/java/com/example/surveyapi/domain/share/domain/entity/Share.java
@@ -0,0 +1,58 @@
+package com.example.surveyapi.domain.share.domain.entity;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+import com.example.surveyapi.domain.share.domain.vo.ShareMethod;
+import com.example.surveyapi.global.model.BaseEntity;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@Table(name = "share")
+public class Share extends BaseEntity {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private Long id;
+	@Column(name = "survey_id", nullable = false)
+	private Long surveyId;
+	@Enumerated(EnumType.STRING)
+	@Column(name = "method", nullable = false)
+	private ShareMethod shareMethod;
+	@Column(name = "link", nullable = false, unique = true)
+	private String link;
+	@OneToMany(mappedBy = "id", cascade = CascadeType.ALL, orphanRemoval = true)
+	private List<Notification> notifications = new ArrayList<>();
+
+	public Share(Long surveyId, ShareMethod shareMethod, String linkUrl) {
+		this.surveyId = surveyId;
+		this.shareMethod = shareMethod;
+		this.link = generateUniqueLink(linkUrl);
+	}
+
+	public static Share create(Long surveyId, ShareMethod shareMethod, String linkUrl) {
+		Share share = new Share(surveyId, shareMethod, linkUrl);
+		return share;
+	}
+
+	private String generateUniqueLink(String linkUrl) {
+		String newUrl =  linkUrl + "/survey/link/"
+			+ UUID.randomUUID().toString().replace("-", "");
+		return newUrl;
+	}
+}
