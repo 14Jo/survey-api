67c4aecd2d90fbefded629ccb659d2ae4869ec45
taeung515 <xodndd515@gmail.com>
2025-07-22 11:18:56 +0900

    feat : 프로젝트 생성 dto 추가

---
+++ src/main/java/com/example/surveyapi/domain/project/domain/dto/request/CreateProjectRequest.java
@@ -0,0 +1,24 @@
+package com.example.surveyapi.domain.project.domain.dto.request;
+
+import java.time.LocalDateTime;
+
+import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+
+@Getter
+public class CreateProjectRequest {
+
+	@NotBlank(message = "이름을 입력해주세요")
+	private String name;
+
+	@NotBlank(message = "설명을 입력해주세요")
+	private String description;
+
+	@NotNull(message = "시작일을 입력해주세요")
+	@Future(message = "시작일은 현재보다 이후여야 합니다.")
+	private LocalDateTime periodStart;
+
+	private LocalDateTime periodEnd;
+}
