aa92913dd0a7c5d36a58e6e7aad8e20c399aeb1e
DongGeun <kk506kk@naver.com>
2025-07-22 12:34:26 +0900

    refactor : SecurityConfig 임시적으로 모든 코드 인증 허용으로 수정

--- src/main/java/com/example/surveyapi/config/security/SecurityConfig.java
+++ src/main/java/com/example/surveyapi/config/security/SecurityConfig.java
@@ -28,7 +28,7 @@ public class SecurityConfig {
             .csrf(AbstractHttpConfigurer::disable)
             .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
             .authorizeHttpRequests(auth -> auth
-                .requestMatchers().permitAll()
+                .requestMatchers("/api/**").permitAll()
                 .anyRequest().authenticated())
             .addFilterBefore(new JwtFilter(jwtUtil), UsernamePasswordAuthenticationFilter.class);
 
