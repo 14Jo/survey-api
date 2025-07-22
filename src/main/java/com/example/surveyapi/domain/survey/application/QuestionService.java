0162cd3045a3b09493f5cd08b38194d7f7bf77dd
이준영 <tera199810@gmail.com>
2025-07-22 18:15:39 +0900

    refactor : 질문 생성 구조 변경
    선택지 엔티티 -> VO로 변경
    그에 따른 질문 생성 로직 변경
    질문 테이블에 선택지 리스트 Json으로 저장

--- src/main/java/com/example/surveyapi/domain/survey/application/QuestionService.java
+++ src/main/java/com/example/surveyapi/domain/survey/application/QuestionService.java
@@ -2,7 +2,6 @@ package com.example.surveyapi.domain.survey.application;
 
 import java.util.List;
 
-import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
@@ -10,7 +9,6 @@ import org.springframework.transaction.annotation.Transactional;
 import com.example.surveyapi.domain.survey.domain.question.Question;
 import com.example.surveyapi.domain.survey.domain.question.QuestionRepository;
 import com.example.surveyapi.domain.survey.application.request.CreateQuestionRequest;
-import com.example.surveyapi.domain.survey.domain.question.event.QuestionCreateEvent;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -21,23 +19,19 @@ import lombok.extern.slf4j.Slf4j;
 public class QuestionService {
 
 	private final QuestionRepository questionRepository;
-	private final ApplicationEventPublisher eventPublisher;
 
 	//TODO 벌크 인서트 고려하기
 	@Transactional(propagation = Propagation.REQUIRES_NEW)
 	public void create(Long surveyId, List<CreateQuestionRequest> questions) {
 		long startTime = System.currentTimeMillis();
-		questions.forEach(question -> {
-			Question q = Question.create(
-				surveyId,
-				question.getContent(),
-				question.getQuestionType(),
-				question.getDisplayOrder(),
-				question.isRequired()
-			);
-			Question save = questionRepository.save(q);
-			eventPublisher.publishEvent(new QuestionCreateEvent(save.getQuestionId(), question.getChoices()));
-		});
+
+		List<Question> questionList = questions.stream().map(question ->
+			Question.create(
+				surveyId, question.getContent(), question.getQuestionType(),
+				question.getDisplayOrder(), question.isRequired(), question.getChoices()
+			)
+		).toList();
+		questionRepository.saveAll(questionList);
 		long endTime = System.currentTimeMillis();
 		log.info("질문 생성 시간 - 총 {} ms", endTime - startTime);
 	}
