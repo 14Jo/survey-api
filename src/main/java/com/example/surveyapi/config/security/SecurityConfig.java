1d2e71e0e7a6995e853e5367d505f86d02fc2edf
DongGeun <kk506kk@naver.com>
2025-07-22 21:36:52 +0900

    feat : 인증이 필요하지 않은 api 설정

--- src/main/java/com/example/surveyapi/config/security/SecurityConfig.java
+++ src/main/java/com/example/surveyapi/config/security/SecurityConfig.java
@@ -28,7 +28,7 @@ public class SecurityConfig {
             .csrf(AbstractHttpConfigurer::disable)
             .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
             .authorizeHttpRequests(auth -> auth
-                .requestMatchers("/api/**").permitAll()
+                .requestMatchers("/api/v1/users/signup", "/api/v1/users/login").permitAll()
                 .anyRequest().authenticated())
             .addFilterBefore(new JwtFilter(jwtUtil), UsernamePasswordAuthenticationFilter.class);
 
