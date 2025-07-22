2a3f89fa40e92a7f70eb284e18dee4911fb16369
easter1201 <kite1201kr@gmail.com>
2025-07-22 20:08:00 +0900

    feat : Notification entity 구현

--- src/main/java/com/example/surveyapi/domain/share/domain/notification/entity/Notification.java
+++ src/main/java/com/example/surveyapi/domain/share/domain/notification/entity/Notification.java
@@ -1,9 +1,13 @@
 package com.example.surveyapi.domain.share.domain.notification.entity;
 
+import java.time.LocalDateTime;
+
+import com.example.surveyapi.domain.share.domain.vo.Status;
 import com.example.surveyapi.global.model.BaseEntity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -20,4 +24,29 @@ public class Notification extends BaseEntity {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "id")
 	private Long id;
+	@Column(name = "share_id", nullable = false)
+	private Long shareId;
+	@Column(name = "recipient_id", nullable = false)
+	private Long recipientId;
+	@Enumerated
+	@Column(name = "status", nullable = false)
+	private Status status;
+	@Column(name = "sent_at")
+	private LocalDateTime sentAt;
+	@Column(name = "failed_reason")
+	private String failedReason;
+
+	public Notification(
+		Long shareId,
+		Long recipientId,
+		Status status,
+		LocalDateTime sentAt,
+		String failedReason
+	) {
+		this.shareId = shareId;
+		this.recipientId = recipientId;
+		this.status = status;
+		this.sentAt = sentAt;
+		this.failedReason = failedReason;
+	}
 }
