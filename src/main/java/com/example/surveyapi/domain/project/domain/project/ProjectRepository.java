e2ed5dc2069cb6fdaab5872194ff8c8360c1218c
taeung515 <xodndd515@gmail.com>
2025-07-22 21:00:04 +0900

    feat : 내가 참여한 프로젝트 목록 조회 API 구현
    QueryDSL 기반 목록 조회, 페이징 및 내 역할 정보 포함
    삭제되지 않은 프로젝트만 조회

--- src/main/java/com/example/surveyapi/domain/project/domain/project/ProjectRepository.java
+++ src/main/java/com/example/surveyapi/domain/project/domain/project/ProjectRepository.java
@@ -1,9 +1,15 @@
 package com.example.surveyapi.domain.project.domain.project;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import com.example.surveyapi.domain.project.application.dto.response.ReadProjectResponse;
+
 public interface ProjectRepository {
 
 	void save(Project project);
 
 	boolean existsByNameAndIsDeletedFalse(String name);
 
+	Page<ReadProjectResponse> findMyProjects(Pageable pageable, Long currentUserId);
 }
