8b84965659a81666513d5cb55b15bca36c34d0bc
DongGeun <kk506kk@naver.com>
2025-07-22 21:09:37 +0900

    feat : Role 추가

--- src/main/java/com/example/surveyapi/config/jwt/JwtUtil.java
+++ src/main/java/com/example/surveyapi/config/jwt/JwtUtil.java
@@ -9,6 +9,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 
+import com.example.surveyapi.domain.user.domain.user.enums.Role;
+
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.Jwts;
