f2d671192d0093e6f373aa2c489654d8ca7a8620
DongGeun <kk506kk@naver.com>
2025-07-23 11:42:28 +0900

    refactor : Command 버전 -> 응용 계층에서 command 활용

--- src/main/java/com/example/surveyapi/domain/user/application/service/UserService.java
+++ src/main/java/com/example/surveyapi/domain/user/application/service/UserService.java
@@ -3,18 +3,16 @@ package com.example.surveyapi.domain.user.application.service;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.example.surveyapi.config.jwt.JwtUtil;
-import com.example.surveyapi.config.security.PasswordEncoder;
-import com.example.surveyapi.domain.user.application.dtos.request.LoginRequest;
 import com.example.surveyapi.domain.user.application.dtos.request.SignupRequest;
+import com.example.surveyapi.global.config.jwt.JwtUtil;
+import com.example.surveyapi.global.config.security.PasswordEncoder;
+import com.example.surveyapi.domain.user.application.dtos.request.LoginRequest;
 import com.example.surveyapi.domain.user.application.dtos.response.LoginResponse;
 import com.example.surveyapi.domain.user.application.dtos.response.MemberResponse;
 import com.example.surveyapi.domain.user.application.dtos.response.SignupResponse;
 import com.example.surveyapi.domain.user.domain.user.User;
 import com.example.surveyapi.domain.user.domain.user.UserRepository;
-import com.example.surveyapi.domain.user.domain.user.vo.Address;
-import com.example.surveyapi.domain.user.domain.user.vo.Auth;
-import com.example.surveyapi.domain.user.domain.user.vo.Profile;
+import com.example.surveyapi.domain.user.domain.user.command.SignupCommand;
 import com.example.surveyapi.global.enums.CustomErrorCode;
 import com.example.surveyapi.global.exception.CustomException;
 
@@ -31,15 +29,17 @@ public class UserService {
     @Transactional
     public SignupResponse signup(SignupRequest request) {
 
-        if (userRepository.existsByEmail(request.getAuth().getEmail())) {
+        SignupCommand command = request.toCommand();
+
+        if (userRepository.existsByEmail(command.getAuth().getEmail())) {
             throw new CustomException(CustomErrorCode.EMAIL_NOT_FOUND);
         }
 
-        User user = from(request, passwordEncoder);
+        User user = User.create(command, passwordEncoder);
 
         User createUser = userRepository.save(user);
 
-        return new SignupResponse(createUser);
+        return SignupResponse.from(createUser);
     }
 
     @Transactional
@@ -47,6 +47,7 @@ public class UserService {
 
         User user = userRepository.findByEmail(request.getEmail())
             .orElseThrow(() -> new CustomException(CustomErrorCode.EMAIL_NOT_FOUND));
+
 
         if (!passwordEncoder.matches(request.getPassword(), user.getAuth().getPassword())) {
             throw new CustomException(CustomErrorCode.WRONG_PASSWORD);
@@ -56,27 +57,8 @@ public class UserService {
 
         String token = jwtUtil.createToken(user.getId(), user.getRole());
 
-        return new LoginResponse(token, member);
+        return LoginResponse.of(token, member);
     }
 
-    public static User from(SignupRequest request, PasswordEncoder passwordEncoder) {
-        Address address = new Address(
-            request.getProfile().getAddress().getProvince(),
-            request.getProfile().getAddress().getDistrict(),
-            request.getProfile().getAddress().getDetailAddress(),
-            request.getProfile().getAddress().getPostalCode()
-        );
 
-        Profile profile = new Profile(
-            request.getProfile().getName(),
-            request.getProfile().getBirthDate(),
-            request.getProfile().getGender(),
-            address);
-
-        Auth auth = new Auth(
-            request.getAuth().getEmail(),
-            passwordEncoder.encode(request.getAuth().getPassword()));
-
-        return new User(auth, profile);
-    }
 }
