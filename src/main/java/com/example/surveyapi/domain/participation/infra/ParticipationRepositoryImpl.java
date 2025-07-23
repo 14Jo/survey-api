8b62271158edc9007d069540d577397d17d519dd
Jang Gyeonghyuk <kcc5107@naver.com>
2025-07-24 03:39:32 +0900

    feat : 여러 설문에 대한 모든 참여 기록 복수 조회 api 구현

--- src/main/java/com/example/surveyapi/domain/participation/infra/ParticipationRepositoryImpl.java
+++ src/main/java/com/example/surveyapi/domain/participation/infra/ParticipationRepositoryImpl.java
@@ -1,5 +1,7 @@
 package com.example.surveyapi.domain.participation.infra;
 
+import java.util.List;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
@@ -24,5 +26,10 @@ public class ParticipationRepositoryImpl implements ParticipationRepository {
 	@Override
 	public Page<Participation> findAll(Long memberId, Pageable pageable) {
 		return jpaParticipationRepository.findAllByMemberIdAndIsDeleted(memberId, false, pageable);
+	}
+
+	@Override
+	public List<Participation> findAllBySurveyIdIn(List<Long> surveyIds) {
+		return jpaParticipationRepository.findAllBySurveyIdInAndIsDeleted(surveyIds, false);
 	}
 }
