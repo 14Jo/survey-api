ae8364cc77d542f9522cbe17d5e6f5f22730ead2
DongGeun <kk506kk@naver.com>
2025-07-23 17:37:18 +0900

    feat : QueryDsl 사용, JPA 사용 및 위치 정리

--- src/main/java/com/example/surveyapi/domain/user/domain/user/UserRepository.java
+++ src/main/java/com/example/surveyapi/domain/user/domain/user/UserRepository.java
@@ -2,9 +2,21 @@ package com.example.surveyapi.domain.user.domain.user;
 
 import java.util.Optional;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+
+import com.example.surveyapi.domain.user.application.dtos.response.UserResponse;
+
 public interface UserRepository {
 
     boolean existsByEmail(String email);
+
     User save(User user);
+
     Optional<User> findByEmail(String email);
+
+    Page<UserResponse> gets(Pageable pageable);
+
+    Optional<User> findById(Long memberId);
 }
