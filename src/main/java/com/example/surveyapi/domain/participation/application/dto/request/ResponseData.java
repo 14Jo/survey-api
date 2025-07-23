6a667663eda7cc4f4fdfbbf163dc8e4fe280ddc1
Jang Gyeonghyuk <kcc5107@naver.com>
2025-07-23 16:53:14 +0900

    del : Response Entity에서 QuestionType 컬럼 삭제, Enum 파일 삭제

--- src/main/java/com/example/surveyapi/domain/participation/application/dto/request/ResponseData.java
+++ src/main/java/com/example/surveyapi/domain/participation/application/dto/request/ResponseData.java
@@ -2,8 +2,6 @@ package com.example.surveyapi.domain.participation.application.dto.request;
 
 import java.util.Map;
 
-import com.example.surveyapi.domain.participation.domain.response.enums.QuestionType;
-
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 
@@ -12,7 +10,5 @@ public class ResponseData {
 
 	@NotNull
 	private Long questionId;
-	@NotNull
-	private QuestionType questionType;
 	private Map<String, Object> answer;
 }
