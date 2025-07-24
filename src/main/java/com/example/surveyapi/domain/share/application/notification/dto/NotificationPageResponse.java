ba3b774868cc45fc6865f89dc0a7aae96e422957
easter1201 <kite1201kr@gmail.com>
2025-07-24 12:53:28 +0900

    refactor : PageResponse에 PageInfo 적용

--- src/main/java/com/example/surveyapi/domain/share/application/notification/dto/NotificationPageResponse.java
+++ src/main/java/com/example/surveyapi/domain/share/application/notification/dto/NotificationPageResponse.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.data.domain.Page;
 
 import com.example.surveyapi.domain.share.domain.notification.entity.Notification;
+import com.example.surveyapi.global.util.PageInfo;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -13,10 +14,7 @@ import lombok.Getter;
 @AllArgsConstructor
 public class NotificationPageResponse {
 	private final List<NotificationResponse> content;
-	private final int page;
-	private final int size;
-	private final long totalElements;
-	private final int totalPages;
+	private final PageInfo pageInfo;
 
 	public static NotificationPageResponse from(Page<Notification> notifications) {
 		List<NotificationResponse> content = notifications
@@ -24,12 +22,13 @@ public class NotificationPageResponse {
 			.map(NotificationResponse::from)
 			.toList();
 
-		return new NotificationPageResponse(
-			content,
-			notifications.getNumber(),
+		PageInfo pageInfo = new PageInfo(
 			notifications.getSize(),
+			notifications.getNumber(),
 			notifications.getTotalElements(),
 			notifications.getTotalPages()
 		);
+
+		return new NotificationPageResponse(content, pageInfo);
 	}
 }
