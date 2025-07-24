1ca209ff10cd6a6d2f65534d116f8b5d2a5eecbc
Jang Gyeonghyuk <kcc5107@naver.com>
2025-07-24 23:26:54 +0900

    test : 도메인 단위 테스트, 서비스 통합 테스트 코드 작성

--- src/main/java/com/example/surveyapi/domain/participation/application/dto/request/CreateParticipationRequest.java
+++ src/main/java/com/example/surveyapi/domain/participation/application/dto/request/CreateParticipationRequest.java
@@ -2,8 +2,10 @@ package com.example.surveyapi.domain.participation.application.dto.request;
 
 import java.util.List;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+@AllArgsConstructor
 @Getter
 public class CreateParticipationRequest {
 
--- src/main/java/com/example/surveyapi/domain/participation/application/dto/request/ResponseData.java
+++ src/main/java/com/example/surveyapi/domain/participation/application/dto/request/ResponseData.java
@@ -3,8 +3,10 @@ package com.example.surveyapi.domain.participation.application.dto.request;
 import java.util.Map;
 
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+@AllArgsConstructor
 @Getter
 public class ResponseData {
 
