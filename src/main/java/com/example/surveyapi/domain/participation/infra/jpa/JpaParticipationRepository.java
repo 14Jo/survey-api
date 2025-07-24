fe7ee8c7c8a82b4540bbaae7c9174712087d1443
Jang Gyeonghyuk <kcc5107@naver.com>
2025-07-24 09:59:44 +0900

    fix : N+1 문제 해결을 위한 fetch join 추가

--- src/main/java/com/example/surveyapi/domain/participation/infra/jpa/JpaParticipationRepository.java
+++ src/main/java/com/example/surveyapi/domain/participation/infra/jpa/JpaParticipationRepository.java
@@ -5,11 +5,15 @@ import java.util.List;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.example.surveyapi.domain.participation.domain.participation.Participation;
 
 public interface JpaParticipationRepository extends JpaRepository<Participation, Long> {
 	Page<Participation> findAllByMemberIdAndIsDeleted(Long memberId, Boolean isDeleted, Pageable pageable);
 
-	List<Participation> findAllBySurveyIdInAndIsDeleted(List<Long> surveyIds, Boolean isDeleted);
+	@Query("select p from Participation p join fetch p.responses where p.surveyId in :surveyIds and p.isDeleted = :isDeleted")
+	List<Participation> findAllBySurveyIdInAndIsDeleted(@Param("surveyIds") List<Long> surveyIds,
+		@Param("isDeleted") Boolean isDeleted);
 }
