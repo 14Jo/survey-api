15e525ee7526c72ead21f9e27807c7bb2657927e
easter1201 <kite1201kr@gmail.com>
2025-07-25 20:01:34 +0900

    refactor : repository 의존 수정, 조회 repository 생성 및 사용

--- src/main/java/com/example/surveyapi/domain/share/application/notification/NotificationService.java
+++ src/main/java/com/example/surveyapi/domain/share/application/notification/NotificationService.java
@@ -1,19 +1,10 @@
 package com.example.surveyapi.domain.share.application.notification;
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.example.surveyapi.domain.share.application.notification.dto.NotificationPageResponse;
-import com.example.surveyapi.domain.share.domain.notification.entity.Notification;
-import com.example.surveyapi.domain.share.domain.notification.repository.NotificationRepository;
-import com.example.surveyapi.domain.share.domain.share.entity.Share;
-import com.example.surveyapi.domain.share.domain.share.repository.ShareRepository;
-import com.example.surveyapi.global.enums.CustomErrorCode;
-import com.example.surveyapi.global.exception.CustomException;
+import com.example.surveyapi.domain.share.domain.notification.repository.query.NotificationQueryRepository;
 
 import lombok.RequiredArgsConstructor;
 
@@ -21,22 +12,10 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 public class NotificationService {
-	private final NotificationRepository notificationRepository;
-	private final ShareRepository shareRepository;
+	private final NotificationQueryRepository notificationQueryRepository;
 
 	public NotificationPageResponse gets(Long shareId, Long requesterId, int page, int size) {
-		Share share = shareRepository.findById(shareId)
-			.orElseThrow(() -> new CustomException(CustomErrorCode.NOT_FOUND_SHARE));
-
-		//TODO : 접근 권한 체크(User 테이블 참조?) - 요청자와 생성자가 일치하는지 + 관리자인지
-
-		Pageable pageable = PageRequest.of(
-			page,
-			size,
-			Sort.by(Sort.Direction.DESC,
-				"sentAt"));
-		Page<Notification> notifications = notificationRepository.findByShareId(shareId, pageable);
-		return NotificationPageResponse.from(notifications);
+		return notificationQueryRepository.findPageByShareId(shareId, requesterId, page, size);
 	}
 
 	private boolean isAdmin(Long userId) {
