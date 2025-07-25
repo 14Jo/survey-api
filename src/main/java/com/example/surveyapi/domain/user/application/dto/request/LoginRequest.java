0eaf0192050fbee3f5bf0fc4415b4382ebf07cca
DongGeun <kk506kk@naver.com>
2025-07-25 10:54:08 +0900

    refactor : dto 리팩토링 (내부 클래스로 이동 및 코드 수정)

---
+++ src/main/java/com/example/surveyapi/domain/user/application/dto/request/LoginRequest.java
@@ -0,0 +1,12 @@
+package com.example.surveyapi.domain.user.application.dto.request;
+
+import lombok.Getter;
+
+
+@Getter
+public class LoginRequest {
+
+    private String email;
+    private String password;
+
+}
