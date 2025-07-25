8357258bd06f44984ff292d19b6bcc782320f2d2
DongGeun <kk506kk@naver.com>
2025-07-25 10:56:17 +0900

    refactor : 의존성 방향을 맞춤

--- src/main/java/com/example/surveyapi/domain/user/infra/user/dsl/QueryDslRepository.java
+++ src/main/java/com/example/surveyapi/domain/user/infra/user/dsl/QueryDslRepository.java
@@ -9,13 +9,10 @@ import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
 
-
-import com.example.surveyapi.domain.user.application.dtos.response.UserResponse;
-import com.example.surveyapi.domain.user.application.dtos.response.vo.AddressResponse;
-import com.example.surveyapi.domain.user.application.dtos.response.vo.ProfileResponse;
+import com.example.surveyapi.domain.user.domain.user.User;
 import com.example.surveyapi.global.enums.CustomErrorCode;
 import com.example.surveyapi.global.exception.CustomException;
-import com.querydsl.core.types.Projections;
+
 import com.querydsl.jpa.impl.JPAQueryFactory;
 
 import lombok.RequiredArgsConstructor;
@@ -26,7 +23,7 @@ public class QueryDslRepository {
 
     private final JPAQueryFactory queryFactory;
 
-    public Page<UserResponse> gets(Pageable pageable) {
+    public Page<User> gets(Pageable pageable) {
 
         Long total = queryFactory.
             select(user.count())
@@ -36,39 +33,18 @@ public class QueryDslRepository {
 
         long totalCount = total != null ? total : 0L;
 
-        if(totalCount == 0L) {
+        if (totalCount == 0L) {
             throw new CustomException(CustomErrorCode.USER_LIST_EMPTY);
         }
 
-        List<UserResponse> userList = queryFactory.select(Projections.constructor(
-                UserResponse.class,
-                user.id,
-                user.auth.email,
-                user.profile.name,
-                user.role,
-                user.grade,
-                user.createdAt,
-                user.updatedAt,
-                Projections.constructor(
-                    ProfileResponse.class,
-                    user.profile.birthDate,
-                    user.profile.gender,
-                    Projections.constructor(
-                        AddressResponse.class,
-                        user.profile.address.province,
-                        user.profile.address.district,
-                        user.profile.address.detailAddress,
-                        user.profile.address.postalCode
-                    )
-                )
-            ))
-            .from(user)
+        List<User> users = queryFactory
+            .selectFrom(user)
             .where(user.isDeleted.eq(false))
             .orderBy(user.createdAt.desc())
             .offset(pageable.getOffset())
             .limit(pageable.getPageSize())
             .fetch();
 
-        return new PageImpl<>(userList, pageable, totalCount);
+        return new PageImpl<>(users, pageable, totalCount);
     }
 }
