d91682640b9f889773722edbe7e958c27293af85
DongGeun <kk506kk@naver.com>
2025-07-28 16:41:21 +0900

    feat : authController와 책임 분리

--- src/main/java/com/example/surveyapi/domain/user/api/internal/UserController.java
+++ src/main/java/com/example/surveyapi/domain/user/api/internal/UserController.java
@@ -7,19 +7,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.surveyapi.domain.user.application.dto.request.LoginRequest;
-import com.example.surveyapi.domain.user.application.dto.request.SignupRequest;
 import com.example.surveyapi.domain.user.application.dto.request.UpdateUserRequest;
-import com.example.surveyapi.domain.user.application.dto.request.UserWithdrawRequest;
 import com.example.surveyapi.domain.user.application.dto.response.UpdateUserResponse;
 import com.example.surveyapi.domain.user.application.dto.response.UserGradeResponse;
-import com.example.surveyapi.domain.user.application.dto.response.LoginResponse;
-import com.example.surveyapi.domain.user.application.dto.response.SignupResponse;
 
 import com.example.surveyapi.domain.user.application.dto.response.UserInfoResponse;
 import com.example.surveyapi.domain.user.application.UserService;
@@ -34,26 +28,6 @@ import lombok.RequiredArgsConstructor;
 public class UserController {
 
     private final UserService userService;
-
-    @PostMapping("/auth/signup")
-    public ResponseEntity<ApiResponse<SignupResponse>> signup(
-        @Valid @RequestBody SignupRequest request
-    ) {
-        SignupResponse signup = userService.signup(request);
-
-        return ResponseEntity.status(HttpStatus.CREATED)
-            .body(ApiResponse.success("회원가입 성공", signup));
-    }
-
-    @PostMapping("/auth/login")
-    public ResponseEntity<ApiResponse<LoginResponse>> login(
-        @Valid @RequestBody LoginRequest request
-    ) {
-        LoginResponse login = userService.login(request);
-
-        return ResponseEntity.status(HttpStatus.OK)
-            .body(ApiResponse.success("로그인 성공", login));
-    }
 
     @GetMapping("/users")
     public ResponseEntity<ApiResponse<Page<UserInfoResponse>>> getUsers(
@@ -94,17 +68,6 @@ public class UserController {
 
         return ResponseEntity.status(HttpStatus.OK)
             .body(ApiResponse.success("회원 정보 수정 성공", update));
-    }
-
-    @PostMapping("/users/withdraw")
-    public ResponseEntity<ApiResponse<Void>> withdraw(
-        @Valid @RequestBody UserWithdrawRequest request,
-        @AuthenticationPrincipal Long userId
-    ) {
-        userService.withdraw(userId, request);
-
-        return ResponseEntity.status(HttpStatus.OK)
-            .body(ApiResponse.success("회원 탈퇴가 완료되었습니다.", null));
     }
 
 }
