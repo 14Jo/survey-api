9ec1a886f542fe0098656e71962bb331c723a0cc
Jindnjs <wlsdnjs0310@naver.com>
2025-07-29 17:00:26 +0900

    refactor : 응답 dto 형식 수정

--- src/main/java/com/example/surveyapi/domain/statistic/infra/adapter/ParticipationServiceAdapter.java
+++ src/main/java/com/example/surveyapi/domain/statistic/infra/adapter/ParticipationServiceAdapter.java
@@ -1,11 +1,16 @@
 package com.example.surveyapi.domain.statistic.infra.adapter;
 
+import java.util.List;
+
 import org.springframework.stereotype.Component;
 
-import com.example.surveyapi.domain.statistic.application.client.ParticipationInfosDto;
+import com.example.surveyapi.domain.statistic.application.client.ParticipationInfoDto;
 import com.example.surveyapi.domain.statistic.application.client.ParticipationRequestDto;
 import com.example.surveyapi.domain.statistic.application.client.ParticipationServicePort;
+import com.example.surveyapi.global.config.client.ExternalApiResponse;
 import com.example.surveyapi.global.config.client.participation.ParticipationApiClient;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -16,9 +21,18 @@ import lombok.extern.slf4j.Slf4j;
 public class ParticipationServiceAdapter implements ParticipationServicePort {
 
 		private final ParticipationApiClient participationApiClient;
+		private final ObjectMapper objectMapper;
 
 		@Override
-		public ParticipationInfosDto getParticipationInfos(String authHeader, ParticipationRequestDto dto) {
-			return participationApiClient.getParticipationInfos(authHeader, dto);
+		public List<ParticipationInfoDto> getParticipationInfos(String authHeader, ParticipationRequestDto dto) {
+			ExternalApiResponse response = participationApiClient.getParticipationInfos(authHeader, dto);
+			Object rawData = response.getOrThrow();
+
+			List<ParticipationInfoDto> responses = objectMapper.convertValue(
+				rawData,
+				new TypeReference<List<ParticipationInfoDto>>() {}
+			);
+
+			return responses;
 		}
 	}
