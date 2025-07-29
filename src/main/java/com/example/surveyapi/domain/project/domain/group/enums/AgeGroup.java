321fde43fc95f9bd6ccbb31a30b2d7e02b32e033
taeung515 <xodndd515@gmail.com>
2025-07-29 16:28:20 +0900

    refactor : 그룹이름을 Enum에서 getter로 사용하도록 리팩토링

--- src/main/java/com/example/surveyapi/domain/project/domain/group/enums/AgeGroup.java
+++ src/main/java/com/example/surveyapi/domain/project/domain/group/enums/AgeGroup.java
@@ -1,8 +1,17 @@
 package com.example.surveyapi.domain.project.domain.group.enums;
 
+import lombok.Getter;
+
+@Getter
 public enum AgeGroup {
-	TWENTIES, // 20대
-	THIRTIES, // 30대
-	FORTIES, // 40대
-	OTHERS // 그 외 연령
+	TWENTIES("20대 그룹"), // 20대
+	THIRTIES("30대 그룹"), // 30대
+	FORTIES("40대 그룹"), // 40대
+	OTHERS("그 외 연령"); // 그 외 연령
+
+	private String groupName;
+
+	AgeGroup(String groupName) {
+		this.groupName = groupName;
+	}
 }
