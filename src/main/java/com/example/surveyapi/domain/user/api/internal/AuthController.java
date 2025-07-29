a80eb8bf335b8978584cb2c85d263de64cbe4321
DongGeun <kk506kk@naver.com>
2025-07-29 16:25:43 +0900

    feat : 로그아웃, 토큰 재발급 추가

--- src/main/java/com/example/surveyapi/domain/user/api/internal/AuthController.java
+++ src/main/java/com/example/surveyapi/domain/user/api/internal/AuthController.java
@@ -5,6 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -55,5 +56,27 @@ public class AuthController {
 
         return ResponseEntity.status(HttpStatus.OK)
             .body(ApiResponse.success("회원 탈퇴가 완료되었습니다.", null));
+    }
+
+    @PostMapping("/auth/logout")
+    public ResponseEntity<ApiResponse<Void>> logout(
+        @RequestHeader("Authorization") String token,
+        @AuthenticationPrincipal Long userId
+    ) {
+        userService.logout(token, userId);
+
+        return ResponseEntity.status(HttpStatus.OK)
+            .body(ApiResponse.success("로그아웃 되었습니다.", null));
+    }
+
+    @PostMapping("/auth/reissue")
+    public ResponseEntity<ApiResponse<LoginResponse>> reissue(
+        @RequestHeader("Authorization") String accessToken,
+        @RequestHeader("RefreshToken") String refreshToken // Bearer 까지 넣어서
+    ) {
+        LoginResponse reissue = userService.reissue(accessToken, refreshToken);
+
+        return ResponseEntity.status(HttpStatus.OK)
+            .body(ApiResponse.success("토큰이 재발급되었습니다.", reissue));
     }
 }
--- src/main/java/com/example/surveyapi/domain/user/application/UserService.java
+++ src/main/java/com/example/surveyapi/domain/user/application/UserService.java
@@ -1,9 +1,11 @@
 package com.example.surveyapi.domain.user.application;
 
+import java.time.Duration;
 import java.util.Optional;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -13,12 +15,7 @@ import com.example.surveyapi.domain.user.application.dto.request.UserWithdrawReq
 import com.example.surveyapi.domain.user.application.dto.response.UpdateUserResponse;
 import com.example.surveyapi.domain.user.application.dto.response.UserGradeResponse;
 import com.example.surveyapi.domain.user.application.dto.response.UserInfoResponse;
-import com.example.surveyapi.domain.user.domain.auth.Auth;
-import com.example.surveyapi.domain.user.domain.auth.enums.Provider;
-import com.example.surveyapi.domain.user.domain.demographics.Demographics;
 import com.example.surveyapi.domain.user.domain.user.enums.Grade;
-import com.example.surveyapi.domain.user.domain.user.vo.Address;
-import com.example.surveyapi.domain.user.domain.user.vo.Profile;
 import com.example.surveyapi.global.config.jwt.JwtUtil;
 import com.example.surveyapi.global.config.security.PasswordEncoder;
 import com.example.surveyapi.domain.user.application.dto.request.LoginRequest;
@@ -29,8 +26,11 @@ import com.example.surveyapi.domain.user.domain.user.UserRepository;
 import com.example.surveyapi.global.enums.CustomErrorCode;
 import com.example.surveyapi.global.exception.CustomException;
 
+import io.jsonwebtoken.Claims;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class UserService {
@@ -38,6 +38,7 @@ public class UserService {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
     private final JwtUtil jwtUtil;
+    private final RedisTemplate<String, String> redisTemplate;
 
     @Transactional
     public SignupResponse signup(SignupRequest request) {
@@ -67,7 +68,7 @@ public class UserService {
         return SignupResponse.from(createUser);
     }
 
-    @Transactional(readOnly = true)
+    @Transactional
     public LoginResponse login(LoginRequest request) {
 
         User user = userRepository.findByEmail(request.getEmail())
@@ -77,9 +78,7 @@ public class UserService {
             throw new CustomException(CustomErrorCode.WRONG_PASSWORD);
         }
 
-        String token = jwtUtil.createToken(user.getId(), user.getRole());
-
-        return LoginResponse.of(token, user);
+        return createAccessAndSaveRefresh(user);
     }
 
     @Transactional(readOnly = true)
@@ -141,4 +140,81 @@ public class UserService {
         user.delete();
     }
 
+    @Transactional
+    public void logout(String bearerAccessToken, Long userId) {
+
+        String accessToken = jwtUtil.subStringToken(bearerAccessToken);
+
+        validateTokenType(accessToken, "access");
+
+        addBlackLists(accessToken);
+
+        String redisKey = "refreshToken" + userId;
+        redisTemplate.delete(redisKey);
+    }
+
+    public LoginResponse reissue(String bearerAccessToken, String bearerRefreshToken) {
+        String accessToken = jwtUtil.subStringToken(bearerAccessToken);
+        String refreshToken = jwtUtil.subStringToken(bearerRefreshToken);
+
+        Claims refreshClaims = jwtUtil.extractClaims(refreshToken);
+
+        validateTokenType(accessToken, "access");
+        validateTokenType(refreshToken, "refresh");
+
+        if (redisTemplate.opsForValue().get("blackListToken" + accessToken) != null) {
+            throw new CustomException(CustomErrorCode.BLACKLISTED_TOKEN);
+        }
+
+        if (jwtUtil.isTokenExpired(accessToken)) {
+            throw new CustomException(CustomErrorCode.ACCESS_TOKEN_NOT_EXPIRED);
+        }
+
+        jwtUtil.validateToken(refreshToken);
+
+        long userId = Long.parseLong(refreshClaims.getSubject());
+        User user = userRepository.findByIdAndIsDeletedFalse(userId)
+            .orElseThrow(() -> new CustomException(CustomErrorCode.USER_NOT_FOUND));
+
+        String redisKey = "refreshToken" + userId;
+        String storedBearerRefreshToken = redisTemplate.opsForValue().get(redisKey);
+
+        if (storedBearerRefreshToken == null) {
+            throw new CustomException(CustomErrorCode.NOT_FOUND_REFRESH_TOKEN);
+        }
+
+        if (!refreshToken.equals(jwtUtil.subStringToken(storedBearerRefreshToken))) {
+            throw new CustomException(CustomErrorCode.MISMATCH_REFRESH_TOKEN);
+        }
+
+        redisTemplate.delete(redisKey);
+
+        return createAccessAndSaveRefresh(user);
+    }
+
+    private LoginResponse createAccessAndSaveRefresh(User user) {
+
+        String newAccessToken = jwtUtil.createAccessToken(user.getId(), user.getRole());
+        String newRefreshToken = jwtUtil.createRefreshToken(user.getId(), user.getRole());
+
+        String redisKey = "refreshToken" + user.getId();
+        redisTemplate.opsForValue().set(redisKey, newRefreshToken, Duration.ofDays(7));
+
+        return LoginResponse.of(newAccessToken, newRefreshToken, user);
+    }
+
+    private void addBlackLists(String accessToken) {
+
+        Long remainingTime = jwtUtil.getExpiration(accessToken);
+        String blackListTokenKey = "blackListToken" + accessToken;
+
+        redisTemplate.opsForValue().set(blackListTokenKey, "logout", Duration.ofMillis(remainingTime));
+    }
+
+    private void validateTokenType(String token, String expectedType) {
+        String type = jwtUtil.extractClaims(token).get("type", String.class);
+        if (!expectedType.equals(type)) {
+            throw new CustomException(CustomErrorCode.INVALID_TOKEN_TYPE);
+        }
+    }
 }
