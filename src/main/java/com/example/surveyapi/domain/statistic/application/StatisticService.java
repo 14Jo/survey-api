e5ad8cf8bf933b94e618fcf05222ad9ef0bc2b1b
Jindnjs <wlsdnjs0310@naver.com>
2025-07-29 10:39:55 +0900

    feat: WebClient를 이용한 외부 도메인 데이터 조회 기능 추가

--- src/main/java/com/example/surveyapi/domain/statistic/application/StatisticService.java
+++ src/main/java/com/example/surveyapi/domain/statistic/application/StatisticService.java
@@ -5,7 +5,9 @@ import java.util.List;
 
 import org.springframework.stereotype.Service;
 
-import com.example.surveyapi.domain.participation.application.ParticipationService;
+import com.example.surveyapi.domain.statistic.application.client.ParticipationInfosDto;
+import com.example.surveyapi.domain.statistic.application.client.ParticipationRequestDto;
+import com.example.surveyapi.domain.statistic.application.client.ParticipationServicePort;
 import com.example.surveyapi.domain.statistic.domain.model.aggregate.Statistics;
 import com.example.surveyapi.domain.statistic.domain.repository.StatisticRepository;
 import com.example.surveyapi.global.enums.CustomErrorCode;
@@ -20,8 +22,7 @@ import lombok.extern.slf4j.Slf4j;
 public class StatisticService {
 
 	private final StatisticRepository statisticRepository;
-	//TODO 임시
-	private final ParticipationService participationService;
+	private final ParticipationServicePort participationServicePort;
 
 	public void create(Long surveyId) {
 		//TODO : survey 유효성 검사
@@ -32,14 +33,16 @@ public class StatisticService {
 		statisticRepository.save(statistic);
 	}
 
-	public void calculateLiveStatistics() {
+	public void calculateLiveStatistics(String authHeader) {
 		//TODO : Survey 도메인으로 부터 진행중인 설문 Id List 받아오기
 		List<Long> surveyIds = new ArrayList<>();
 		surveyIds.add(1L);
 		surveyIds.add(2L);
 		surveyIds.add(3L);
 
-		participationService.getAllBySurveyIds(surveyIds);
+		ParticipationRequestDto request = new ParticipationRequestDto(surveyIds);
+		ParticipationInfosDto participationInfos = participationServicePort.getParticipationInfos(authHeader, request);
 
+		log.info("ParticipationInfos: {}", participationInfos);
 	}
 }
--- src/main/java/com/example/surveyapi/global/config/client/example/ExampleClientConfig.java
+++ src/main/java/com/example/surveyapi/global/config/client/example/ExampleClientConfig.java
@@ -1,4 +1,4 @@
-package com.example.surveyapi.global.config.client;
+package com.example.surveyapi.global.config.client.example;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
