5b05716882117eb74d22854d7237f2ea67d52048
이준영 <tera199810@gmail.com>
2025-07-30 11:45:49 +0900

    hotfix : 테스트 코드 수정

--- src/test/java/com/example/surveyapi/TestConfig.java
+++ src/test/java/com/example/surveyapi/TestConfig.java
@@ -1,9 +1,6 @@
 package com.example.surveyapi;
 
 import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Primary;
-import org.springframework.test.context.TestPropertySource;
 
 @TestConfiguration
 public class TestConfig {
--- src/test/java/com/example/surveyapi/domain/survey/application/SurveyQueryServiceTest.java
+++ src/test/java/com/example/surveyapi/domain/survey/application/SurveyQueryServiceTest.java
@@ -24,7 +24,6 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.*;
 
 @SpringBootTest
-@TestPropertySource(properties = "SECRET_KEY=12345678901234567890123456789012")
 @Transactional
 class SurveyQueryServiceTest {
 
