bc03706a535411e6ffb4b7ddb68b0e3382164af0
DongGeun <kk506kk@naver.com>
2025-07-30 16:31:21 +0900

    feat : 기본 생성자 추가

--- src/main/java/com/example/surveyapi/domain/user/application/client/MyProjectRoleResponse.java
+++ src/main/java/com/example/surveyapi/domain/user/application/client/MyProjectRoleResponse.java
@@ -1,8 +1,11 @@
 package com.example.surveyapi.domain.user.application.client;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@NoArgsConstructor
 @Getter
 public class MyProjectRoleResponse {
+    private Long projectId;
     private String myRole;
 }
