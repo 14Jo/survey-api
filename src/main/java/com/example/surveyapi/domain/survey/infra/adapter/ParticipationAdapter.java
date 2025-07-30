239ad492aedd849dd22a3f67a8cd59aa7ca9a89d
이준영 <tera199810@gmail.com>
2025-07-30 16:31:59 +0900

    feat : 참여자 수 조회 기능 구현
    리스트 조회에 참여자 수 조회 기능 구현

--- src/main/java/com/example/surveyapi/domain/survey/infra/adapter/ParticipationAdapter.java
+++ src/main/java/com/example/surveyapi/domain/survey/infra/adapter/ParticipationAdapter.java
@@ -9,10 +9,6 @@ import com.example.surveyapi.domain.survey.application.client.ParticipationCount
 import com.example.surveyapi.domain.survey.application.client.ParticipationPort;
 import com.example.surveyapi.global.config.client.ExternalApiResponse;
 import com.example.surveyapi.global.config.client.participation.ParticipationApiClient;
-import com.example.surveyapi.global.enums.CustomErrorCode;
-import com.example.surveyapi.global.exception.CustomException;
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -23,29 +19,14 @@ import lombok.extern.slf4j.Slf4j;
 public class ParticipationAdapter implements ParticipationPort {
 
 	private final ParticipationApiClient participationApiClient;
-	private final ObjectMapper objectMapper;
 
 	@Override
 	public ParticipationCountDto getParticipationCounts(String authHeader, List<Long> surveyIds) {
 		ExternalApiResponse participationCounts = participationApiClient.getParticipationCounts(authHeader, surveyIds);
 
-		Map<Long, Long> rawData = convertToMap(participationCounts.getData());
+		@SuppressWarnings("unchecked")
+		Map<String, Integer> rawData = (Map<String, Integer>)participationCounts.getData();
 
 		return ParticipationCountDto.of(rawData);
-	}
-
-	@SuppressWarnings("unchecked")
-	private Map<Long, Long> convertToMap(Object data) {
-		if (data instanceof Map) {
-			return (Map<Long, Long>)data;
-		}
-
-		try {
-			return objectMapper.convertValue(data, new TypeReference<Map<Long, Long>>() {
-			});
-		} catch (Exception e) {
-			log.error("Map<Long, Long>으로 변환 실패: {}", data, e);
-			throw new CustomException(CustomErrorCode.SERVER_ERROR, "참여자 수 데이터 변환 실패");
-		}
 	}
 }
