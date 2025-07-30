601961d0b365153452b58e38860fa9e95ff1142b
DongGeun <kk506kk@naver.com>
2025-07-30 16:32:07 +0900

    feat : 예외 처리 확인 , 로그 추가

--- src/main/java/com/example/surveyapi/domain/user/infra/adapter/ProjectAdapter.java
+++ src/main/java/com/example/surveyapi/domain/user/infra/adapter/ProjectAdapter.java
@@ -9,7 +9,9 @@ import com.example.surveyapi.domain.user.application.client.ProjectPort;
 import com.example.surveyapi.global.config.client.project.ProjectApiClient;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class ProjectAdapter implements ProjectPort {
@@ -18,6 +20,12 @@ public class ProjectAdapter implements ProjectPort {
 
     @Override
     public List<MyProjectRoleResponse> getProjectMyRole(String authHeader, Long userId) {
-        return projectApiClient.getProjectMyRole(authHeader,userId);
+        try {
+            log.info("ProjectApiClient 호출 시도");
+            return projectApiClient.getProjectMyRole(authHeader, userId);
+        } catch (Exception e) {
+            log.error("ProjectApiClient 호출 실패", e);  // 여기서 예외 내용을 로그로 확인
+            throw e;
+        }
     }
 }
