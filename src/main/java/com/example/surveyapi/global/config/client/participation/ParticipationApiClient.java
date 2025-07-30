4ff6b05286321f7d8743eaaaf2f72ec8abaa440f
DongGeun <kk506kk@naver.com>
2025-07-30 14:55:41 +0900

    feat : 외부 API Client 생성 (프로젝트, 참여)

--- src/main/java/com/example/surveyapi/global/config/client/participation/ParticipationApiClient.java
+++ src/main/java/com/example/surveyapi/global/config/client/participation/ParticipationApiClient.java
@@ -1,19 +1,30 @@
 package com.example.surveyapi.global.config.client.participation;
 
+import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.service.annotation.GetExchange;
 import org.springframework.web.service.annotation.HttpExchange;
 import org.springframework.web.service.annotation.PostExchange;
 
 import com.example.surveyapi.domain.statistic.application.client.ParticipationInfosDto;
 import com.example.surveyapi.domain.statistic.application.client.ParticipationRequestDto;
+import com.example.surveyapi.domain.user.application.client.UserSurveyStatusResponse;
 
 @HttpExchange
 public interface ParticipationApiClient {
 
-	@PostExchange("/api/v1/surveys/participations")
-	ParticipationInfosDto getParticipationInfos (
-		@RequestHeader("Authorization") String authHeader,
-		@RequestBody ParticipationRequestDto dto
-	);
+    @PostExchange("/api/v1/surveys/participations")
+    ParticipationInfosDto getParticipationInfos(
+        @RequestHeader("Authorization") String authHeader,
+        @RequestBody ParticipationRequestDto dto
+    );
+
+    @GetExchange("/api/v1/members/me/participations")
+    Page<UserSurveyStatusResponse> getSurveyStatus(
+        @RequestHeader("Authorization") String authHeader,
+        @RequestParam Long userId,
+        @RequestParam("page") int page,
+        @RequestParam("size") int size);
 }
