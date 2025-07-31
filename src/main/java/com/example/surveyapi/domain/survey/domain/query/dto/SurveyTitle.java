697bf58b26ac8a43a6f97bce1ab8035d0c3e2ce4
이준영 <tera199810@gmail.com>
2025-07-31 09:39:00 +0900

    refactor : 요구사항을 위한 응답 객체 변경

--- src/main/java/com/example/surveyapi/domain/survey/domain/query/dto/SurveyTitle.java
+++ src/main/java/com/example/surveyapi/domain/survey/domain/query/dto/SurveyTitle.java
@@ -2,6 +2,7 @@ package com.example.surveyapi.domain.survey.domain.query.dto;
 
 import com.example.surveyapi.domain.survey.domain.survey.enums.SurveyStatus;
 import com.example.surveyapi.domain.survey.domain.survey.vo.SurveyDuration;
+import com.example.surveyapi.domain.survey.domain.survey.vo.SurveyOption;
 
 import lombok.AccessLevel;
 import lombok.Getter;
@@ -12,13 +13,15 @@ import lombok.NoArgsConstructor;
 public class SurveyTitle {
 	private Long surveyId;
 	private String title;
+	private SurveyOption option;
 	private SurveyStatus status;
 	private SurveyDuration duration;
 
-	public static SurveyTitle of(Long surveyId, String title, SurveyStatus status, SurveyDuration duration) {
+	public static SurveyTitle of(Long surveyId, String title, SurveyOption option, SurveyStatus status, SurveyDuration duration) {
 		SurveyTitle surveyTitle = new SurveyTitle();
 		surveyTitle.surveyId = surveyId;
 		surveyTitle.title = title;
+		surveyTitle.option = option;
 		surveyTitle.status = status;
 		surveyTitle.duration = duration;
 		return surveyTitle;
