3ed257a29019ce7aa99f0f2a4d8b109c8dbb9f13
이준영 <tera199810@gmail.com>
2025-07-31 18:01:35 +0900

    refactor : 설문 시작 시 제약 조건 추가
    논리적 삭제 필터링
    상태 코드에 따른 업데이트 허용여부
    시작일 종료일 기준 강화

--- src/main/java/com/example/surveyapi/domain/survey/application/client/ProjectPort.java
+++ src/main/java/com/example/surveyapi/domain/survey/application/client/ProjectPort.java
@@ -3,4 +3,6 @@ package com.example.surveyapi.domain.survey.application.client;
 public interface ProjectPort {
 
 	ProjectValidDto getProjectMembers(Long projectId, Long userId);
+	
+	ProjectStateDto getProjectState(Long projectId);
 }
