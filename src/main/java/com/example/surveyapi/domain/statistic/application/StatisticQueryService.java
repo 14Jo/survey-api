838ce54f4219ea2deee4d876d1a056191f33da61
Jindnjs <wlsdnjs0310@naver.com>
2025-08-01 14:07:46 +0900

    add : 통계 조회용 파일 생성

---
+++ src/main/java/com/example/surveyapi/domain/statistic/application/StatisticQueryService.java
@@ -0,0 +1,10 @@
+package com.example.surveyapi.domain.statistic.application;
+
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class StatisticQueryService {
+}
