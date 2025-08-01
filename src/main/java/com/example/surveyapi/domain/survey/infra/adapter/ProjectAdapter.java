dd7a588136d416ce927e193f3e3ca4d04a820254
이준영 <tera199810@gmail.com>
2025-08-01 13:25:10 +0900

    fix : 테스트 코드 수정

--- src/main/java/com/example/surveyapi/domain/survey/infra/adapter/ProjectAdapter.java
+++ src/main/java/com/example/surveyapi/domain/survey/infra/adapter/ProjectAdapter.java
@@ -3,7 +3,6 @@ package com.example.surveyapi.domain.survey.infra.adapter;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Component;
 
