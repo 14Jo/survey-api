8c4ea7e1438413ef651b2f1904f7b7b10c3556ed
DongGeun <kk506kk@naver.com>
2025-08-01 09:18:07 +0900

    feat : 사용자 정보 요청 api

--- src/main/java/com/example/surveyapi/global/config/client/user/KakaoApiClient.java
+++ src/main/java/com/example/surveyapi/global/config/client/user/KakaoApiClient.java
@@ -1,23 +1,30 @@
 package com.example.surveyapi.global.config.client.user;
 
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.service.annotation.GetExchange;
 import org.springframework.web.service.annotation.HttpExchange;
 import org.springframework.web.service.annotation.PostExchange;
 
-import com.example.surveyapi.domain.user.application.dto.response.KakaoOauthResponse;
+import com.example.surveyapi.domain.user.application.client.KakaoAccessResponse;
+import com.example.surveyapi.domain.user.application.client.KakaoUserInfoResponse;
 
-@HttpExchange(url = "https://kauth.kakao.com")
+@HttpExchange
 public interface KakaoApiClient {
 
     @PostExchange(
-        url = "/oauth/token",
+        url = "https://kauth.kakao.com/oauth/token",
         contentType = "application/x-www-form-urlencoded;charset=utf-8")
-    KakaoOauthResponse getKakaoAccessToken(
+    KakaoAccessResponse getKakaoAccessToken(
         @RequestParam("grant_type") String grant_type ,
         @RequestParam("client_id") String client_id,
         @RequestParam("redirect_uri") String redirect_uri,
         @RequestParam("code") String code
     );
+
+    @GetExchange(url = "https://kapi.kakao.com/v2/user/me")
+    KakaoUserInfoResponse getKakaoUserInfo(
+        @RequestHeader("Authorization") String accessToken);
 
 
 
