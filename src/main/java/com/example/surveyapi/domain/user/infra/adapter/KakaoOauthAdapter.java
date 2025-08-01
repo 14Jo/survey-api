7d63155f3c105cc84dfef9ed6a41ec17ea928665
DongGeun <kk506kk@naver.com>
2025-08-01 09:15:51 +0900

    feat : port, adapter에 사용자 정보 요청 기능 추가

--- src/main/java/com/example/surveyapi/domain/user/infra/adapter/KakaoOauthAdapter.java
+++ src/main/java/com/example/surveyapi/domain/user/infra/adapter/KakaoOauthAdapter.java
@@ -3,8 +3,9 @@ package com.example.surveyapi.domain.user.infra.adapter;
 import org.springframework.stereotype.Component;
 
 import com.example.surveyapi.domain.user.application.client.KakaoOauthPort;
-import com.example.surveyapi.domain.user.application.dto.request.KakaoOauthRequest;
-import com.example.surveyapi.domain.user.application.dto.response.KakaoOauthResponse;
+import com.example.surveyapi.domain.user.application.client.KakaoOauthRequest;
+import com.example.surveyapi.domain.user.application.client.KakaoAccessResponse;
+import com.example.surveyapi.domain.user.application.client.KakaoUserInfoResponse;
 import com.example.surveyapi.global.config.client.user.KakaoApiClient;
 
 import lombok.RequiredArgsConstructor;
@@ -16,9 +17,14 @@ public class KakaoOauthAdapter implements KakaoOauthPort {
     private final KakaoApiClient kakaoApiClient;
 
     @Override
-    public KakaoOauthResponse getKakaoOauthResponse(KakaoOauthRequest request) {
+    public KakaoAccessResponse getKakaoAccess(KakaoOauthRequest request) {
         return kakaoApiClient.getKakaoAccessToken(
             request.getGrant_type(), request.getClient_id(),
             request.getRedirect_uri(), request.getCode());
+    }
+
+    @Override
+    public KakaoUserInfoResponse getKakaoUserInfo(String accessToken) {
+        return kakaoApiClient.getKakaoUserInfo(accessToken);
     }
 }
