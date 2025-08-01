1582dbac08a65234c320a5c3c669c24f0d5395a2
DongGeun <kk506kk@naver.com>
2025-08-01 09:15:51 +0900

    feat : port, adapter에 사용자 정보 요청 기능 추가

--- src/main/java/com/example/surveyapi/domain/user/application/client/KakaoOauthPort.java
+++ src/main/java/com/example/surveyapi/domain/user/application/client/KakaoOauthPort.java
@@ -1,8 +1,8 @@
 package com.example.surveyapi.domain.user.application.client;
 
-import com.example.surveyapi.domain.user.application.dto.request.KakaoOauthRequest;
-import com.example.surveyapi.domain.user.application.dto.response.KakaoOauthResponse;
-
 public interface KakaoOauthPort {
-    KakaoOauthResponse getKakaoOauthResponse(KakaoOauthRequest request);
+    KakaoAccessResponse getKakaoAccess(KakaoOauthRequest request);
+
+    KakaoUserInfoResponse getKakaoUserInfo(String accessToken);
+
 }
