4f704009a73c89874cd755bd454637951f1bf515
taeung515 <xodndd515@gmail.com>
2025-08-01 12:13:51 +0900

    feat : 회원탈퇴 이벤트 구독
    findProjectsByMember(event.getUserId()),findProjectsByManager(event.getUserId()) project join member, project join manager List<Project>를 조회하여 루트 애그리거트인 Project도메인에서 삭제 하도록 구현
    
    카테시안 곱 최소화 하기 위하여 멤버조인, 매니저조인 두개의 쿼리로 분할

---
+++ src/main/java/com/example/surveyapi/domain/project/application/event/UserEventHandler.java
@@ -0,0 +1,38 @@
+package com.example.surveyapi.domain.project.application.event;
+
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.event.TransactionalEventListener;
+
+import com.example.surveyapi.domain.project.domain.project.entity.Project;
+import com.example.surveyapi.domain.project.domain.project.repository.ProjectRepository;
+import com.example.surveyapi.domain.user.domain.user.event.UserWithdrawEvent;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class UserEventHandler {
+
+	private final ProjectRepository projectRepository;
+
+	@TransactionalEventListener
+	public void handleUserWithdrawEvent(UserWithdrawEvent event) {
+		log.debug("회원 탈퇴 이벤트 수신 userId: {}", event.getUserId());
+
+		List<Project> projectsByMember = projectRepository.findProjectsByMember(event.getUserId());
+		for (Project project : projectsByMember) {
+			project.removeMember(event.getUserId());
+		}
+
+		List<Project> projectsByManager = projectRepository.findProjectsByManager(event.getUserId());
+		for (Project project : projectsByManager) {
+			project.removeManager(event.getUserId());
+		}
+
+		log.debug("회원 탈퇴 처리 완료 userId: {}", event.getUserId());
+	}
+}
