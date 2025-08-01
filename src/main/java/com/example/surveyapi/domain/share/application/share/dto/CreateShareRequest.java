f6b1a18ca9afaeb947d09ef9522aff8d214f25d2
easter1201 <kite1201kr@gmail.com>
2025-08-01 10:35:10 +0900

    feat : 엔티티 수정에 따른 dto, controller 수정

--- src/main/java/com/example/surveyapi/domain/share/application/share/dto/CreateShareRequest.java
+++ src/main/java/com/example/surveyapi/domain/share/application/share/dto/CreateShareRequest.java
@@ -1,6 +1,10 @@
 package com.example.surveyapi.domain.share.application.share.dto;
 
+import java.time.LocalDateTime;
+
+import com.example.surveyapi.domain.share.application.share.ShareService;
 import com.example.surveyapi.domain.share.domain.notification.vo.ShareMethod;
+import com.example.surveyapi.domain.share.domain.share.vo.ShareSourceType;
 
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
@@ -10,7 +14,9 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class CreateShareRequest {
 	@NotNull
-	private Long surveyId;
+	private ShareSourceType sourceType;
 	@NotNull
-	private ShareMethod shareMethod;
+	private Long sourceId;
+	@NotNull
+	private LocalDateTime expirationDate;
 }
