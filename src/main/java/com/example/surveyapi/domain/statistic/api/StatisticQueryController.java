fe474f6cf621af8c7345ca78e922e57c2cf75f33
Jindnjs <wlsdnjs0310@naver.com>
2025-08-06 04:37:57 +0900

    feat : 통계 조회 기능 추가

--- src/main/java/com/example/surveyapi/domain/statistic/api/StatisticQueryController.java
+++ src/main/java/com/example/surveyapi/domain/statistic/api/StatisticQueryController.java
@@ -1,8 +1,15 @@
 package com.example.surveyapi.domain.statistic.api;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.surveyapi.domain.statistic.application.StatisticQueryService;
+import com.example.surveyapi.domain.statistic.application.dto.response.StatisticDetailResponse;
+import com.example.surveyapi.global.util.ApiResponse;
 
 import lombok.RequiredArgsConstructor;
 
@@ -11,4 +18,15 @@ import lombok.RequiredArgsConstructor;
 public class StatisticQueryController {
 
 	private final StatisticQueryService statisticQueryService;
+
+	@GetMapping("/api/v2/surveys/{surveyId}/statistics/live")
+	public ResponseEntity<ApiResponse<StatisticDetailResponse>> getLiveStatistics(
+		@PathVariable Long surveyId,
+		@RequestHeader("Authorization") String authHeader
+	) {
+		StatisticDetailResponse liveStatistics = statisticQueryService.getLiveStatistics(authHeader, surveyId);
+
+		return ResponseEntity.status(HttpStatus.OK)
+			.body(ApiResponse.success("통계 조회 성공.", liveStatistics));
+	}
 }
