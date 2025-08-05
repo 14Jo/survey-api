811ccfb95316259d94a4a1f49cbb3c60ad460d7e
easter1201 <kite1201kr@gmail.com>
2025-08-05 20:22:08 +0900

    feat : Event listener 구현

--- src/main/java/com/example/surveyapi/domain/share/application/event/ShareEventListener.java
+++ src/main/java/com/example/surveyapi/domain/share/application/event/ShareEventListener.java
@@ -1,10 +1,16 @@
 package com.example.surveyapi.domain.share.application.event;
 
+import java.time.LocalDateTime;
+import java.util.Collections;
+import java.util.List;
+
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
-import com.example.surveyapi.domain.share.application.notification.NotificationService;
-import com.example.surveyapi.domain.share.domain.share.event.ShareCreateEvent;
+import com.example.surveyapi.domain.share.application.share.ShareService;import com.example.surveyapi.domain.share.domain.share.event.ShareCreateEvent;
+import com.example.surveyapi.domain.share.domain.share.vo.ShareMethod;
+import com.example.surveyapi.domain.share.domain.share.vo.ShareSourceType;
+import com.example.surveyapi.global.event.SurveyActivateEvent;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -13,16 +19,36 @@ import lombok.extern.slf4j.Slf4j;
 @Component
 @RequiredArgsConstructor
 public class ShareEventListener {
-	private final NotificationService notificationService;
+	private final ShareService shareService;
 
 	@EventListener
-	public void handleShareCreated(ShareCreateEvent event) {
-		log.info("알림 생성 이벤트 수신: shareId = {}", event.getShare().getId());
+	public void handleSurveyActivateEvent(SurveyActivateEvent event) {
+		log.info("설문 공유 작업 생성 시작: {}", event.getSurveyId());
 
-		try {
-			notificationService.create(event.getShare(), event.getCreatorId());
-		} catch (Exception e) {
-			log.error("알림 생성 중 오류 발생", e);
-		}
+		List<Long> recipientIds = Collections.emptyList();
+
+		shareService.createShare(
+			ShareSourceType.SURVEY,
+			event.getSurveyId(),
+			event.getCreatorID(),
+			ShareMethod.URL,
+			event.getEndTime(),
+			recipientIds,
+			LocalDateTime.now()
+		);
+	}
+
+	@EventListener
+	public void handleProjectManagerEvent() {
+		log.info("프로젝트 매니저 공유 작업 생성 시작: {}", event.getProjectId());
+
+		// TODO : Project Event 생성 후 작업
+	}
+
+	@EventListener
+	public void handleProjectMemberEvent() {
+		log.info("프로젝트 참여 인원 공유 작업 생성 시작: {}", event.getProjectId());
+
+		// TODO : Project Event 생성 후 작업
 	}
 }
