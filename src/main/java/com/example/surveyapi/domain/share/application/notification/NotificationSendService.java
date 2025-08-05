e445d70cbe5eef4a837028832b3a19d9ceae45fd
easter1201 <kite1201kr@gmail.com>
2025-08-05 20:20:55 +0900

    feat : domain service 내 notifyAt 추가

--- src/main/java/com/example/surveyapi/domain/share/application/notification/NotificationSendService.java
+++ src/main/java/com/example/surveyapi/domain/share/application/notification/NotificationSendService.java
@@ -1,7 +1,5 @@
 package com.example.surveyapi.domain.share.application.notification;
 
-import java.util.List;
-
 import com.example.surveyapi.domain.share.domain.notification.entity.Notification;
 
 public interface NotificationSendService {
