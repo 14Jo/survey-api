907df16cc0b666e0d240f980aad1d58a56551f70
easter1201 <kite1201kr@gmail.com>
2025-08-05 18:17:36 +0900

    feat : 알림 발송 관리 위한 sender 추가

--- src/main/java/com/example/surveyapi/domain/share/infra/notification/sender/NotificationEmailSender.java
+++ src/main/java/com/example/surveyapi/domain/share/infra/notification/sender/NotificationEmailSender.java
@@ -1,4 +1,13 @@
 package com.example.surveyapi.domain.share.infra.notification.sender;
 
+import org.springframework.stereotype.Component;
+
+import com.example.surveyapi.domain.share.domain.notification.entity.Notification;
+
+@Component("EMAIL")
 public class NotificationEmailSender implements NotificationSender {
+	@Override
+	public void send(Notification notification) {
+		// TODO : 이메일 실제 전송
+	}
 }
