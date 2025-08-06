6fc992139e6bd51ff9b53ae9f0ac464ecca1e4e3
easter1201 <kite1201kr@gmail.com>
2025-08-06 16:51:53 +0900

    feat : 로그 추가

--- src/main/java/com/example/surveyapi/domain/share/infra/notification/sender/NotificationPushSender.java
+++ src/main/java/com/example/surveyapi/domain/share/infra/notification/sender/NotificationPushSender.java
@@ -4,10 +4,14 @@ import org.springframework.stereotype.Component;
 
 import com.example.surveyapi.domain.share.domain.notification.entity.Notification;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @Component("PUSH")
 public class NotificationPushSender implements NotificationSender {
 	@Override
 	public void send(Notification notification) {
+		log.info("PUSH 전송: {}", notification.getId());
 		// TODO : 실제 PUSH 전송
 	}
 }
