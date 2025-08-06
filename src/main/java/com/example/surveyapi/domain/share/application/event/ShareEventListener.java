afbed9b2755f5b19379fc33e8cee60f497b0d364
easter1201 <kite1201kr@gmail.com>
2025-08-06 16:53:38 +0900

    feat : 프로젝트 공유 작업 생성 기능 추가

--- src/main/java/com/example/surveyapi/domain/share/application/event/ShareEventListener.java
+++ src/main/java/com/example/surveyapi/domain/share/application/event/ShareEventListener.java
@@ -7,10 +7,14 @@ import java.util.List;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
-import com.example.surveyapi.domain.share.application.share.ShareService;import com.example.surveyapi.domain.share.domain.share.event.ShareCreateEvent;
+import com.example.surveyapi.domain.share.application.share.ShareService;
+import com.example.surveyapi.domain.share.domain.share.entity.Share;
 import com.example.surveyapi.domain.share.domain.share.vo.ShareMethod;
 import com.example.surveyapi.domain.share.domain.share.vo.ShareSourceType;
 import com.example.surveyapi.global.event.SurveyActivateEvent;
+import com.example.surveyapi.global.event.project.ProjectDeletedEvent;
+import com.example.surveyapi.global.event.project.ProjectManagerAddedEvent;
+import com.example.surveyapi.global.event.project.ProjectMemberAddedEvent;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -31,7 +35,7 @@ public class ShareEventListener {
 			ShareSourceType.SURVEY,
 			event.getSurveyId(),
 			event.getCreatorID(),
-			ShareMethod.URL,
+			ShareMethod.URL, // TODO: 변경 방식은 추후 변경
 			event.getEndTime(),
 			recipientIds,
 			LocalDateTime.now()
@@ -39,16 +43,48 @@ public class ShareEventListener {
 	}
 
 	@EventListener
-	public void handleProjectManagerEvent() {
+	public void handleProjectManagerEvent(ProjectManagerAddedEvent event) {
 		log.info("프로젝트 매니저 공유 작업 생성 시작: {}", event.getProjectId());
 
-		// TODO : Project Event 생성 후 작업
+		List<Long> recipientIds = Collections.emptyList();
+
+		shareService.createShare(
+			ShareSourceType.PROJECT_MANAGER,
+			event.getProjectId(),
+			event.getCreatorId(),
+			ShareMethod.URL, // TODO: 변경 방식은 추후 변경
+			event.getPeriodEnd(),
+			recipientIds,
+			LocalDateTime.now()
+		);
 	}
 
 	@EventListener
-	public void handleProjectMemberEvent() {
+	public void handleProjectMemberEvent(ProjectMemberAddedEvent event) {
 		log.info("프로젝트 참여 인원 공유 작업 생성 시작: {}", event.getProjectId());
 
-		// TODO : Project Event 생성 후 작업
+		List<Long> recipientIds = Collections.emptyList();
+
+		shareService.createShare(
+			ShareSourceType.PROJECT_MEMBER,
+			event.getProjectId(),
+			event.getCreatorId(),
+			ShareMethod.URL, // TODO : 변경 방식은 추후 변경
+			event.getPeriodEnd(),
+			recipientIds,
+			LocalDateTime.now()
+		);
+	}
+
+	@EventListener
+	public void handleProjectDeleteEvent(ProjectDeletedEvent event) {
+		log.info("프로젝트 삭제 시작: {}", event.getProjectId());
+
+		List<Share> shares = shareService.getShareBySource(event.getProjectId());
+
+		for (Share share: shares) {
+			shareService.delete(share.getId(), event.getUserId());
+		}
+		log.info("프로젝트 삭제 완료");
 	}
 }
