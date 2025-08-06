f2b978893c1274eba4b3f2942bfd646ea5457f62
easter1201 <kite1201kr@gmail.com>
2025-08-06 18:52:37 +0900

    feat : 누락 내용 추가

--- src/main/java/com/example/surveyapi/domain/share/infra/notification/sender/NotificationSendServiceImpl.java
+++ src/main/java/com/example/surveyapi/domain/share/infra/notification/sender/NotificationSendServiceImpl.java
@@ -1,10 +1,13 @@
 package com.example.surveyapi.domain.share.infra.notification.sender;
 
+import org.springframework.stereotype.Service;
+
 import com.example.surveyapi.domain.share.application.notification.NotificationSendService;
 import com.example.surveyapi.domain.share.domain.notification.entity.Notification;
 
 import lombok.RequiredArgsConstructor;
 
+@Service
 @RequiredArgsConstructor
 public class NotificationSendServiceImpl implements NotificationSendService {
 	private final NotificationFactory factory;
