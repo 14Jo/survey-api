6803c58f956825c3256f2259fb8c3ffbe3a2d9ea
easter1201 <kite1201kr@gmail.com>
2025-08-06 16:51:53 +0900

    feat : 로그 추가

--- src/main/java/com/example/surveyapi/domain/share/infra/notification/sender/NotificationEmailSender.java
+++ src/main/java/com/example/surveyapi/domain/share/infra/notification/sender/NotificationEmailSender.java
@@ -4,10 +4,14 @@ import org.springframework.stereotype.Component;
 
 import com.example.surveyapi.domain.share.domain.notification.entity.Notification;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @Component("EMAIL")
 public class NotificationEmailSender implements NotificationSender {
 	@Override
 	public void send(Notification notification) {
+		log.info("이메일 전송: {}", notification.getId());
 		// TODO : 이메일 실제 전송
 	}
 }
