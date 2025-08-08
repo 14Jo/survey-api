e04f6fca2eca9f7b03497788703d92f3acc92a60
taeung515 <xodndd515@gmail.com>
2025-08-08 15:04:53 +0900

    refactor : 검색 요청 dto에 NotBlank 제약 추가
    - 기존 keyword입력하지 않으면 전체 조회 하도록 하였으나, 병목현상 발생으로 no offset 커서 방식으로 따로 api 구현 예정

--- src/main/java/com/example/surveyapi/domain/project/application/dto/request/SearchProjectRequest.java
+++ src/main/java/com/example/surveyapi/domain/project/application/dto/request/SearchProjectRequest.java
@@ -1,5 +1,6 @@
 package com.example.surveyapi.domain.project.application.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
@@ -8,5 +9,6 @@ import lombok.Setter;
 @Setter
 public class SearchProjectRequest {
 	@Size(min = 3, message = "검색어는 최소 3글자 이상이어야 합니다.")
+	@NotBlank(message = "검색어를 입력해주세요")
 	private String keyword;
 }
