becf1b9d3794462e7f15c97029db0827a83af513
easter1201 <kite1201kr@gmail.com>
2025-08-08 19:01:53 +0900

    feat : 이메일 전송 구현

--- src/main/java/com/example/surveyapi/domain/share/infra/notification/sender/NotificationEmailSender.java
+++ src/main/java/com/example/surveyapi/domain/share/infra/notification/sender/NotificationEmailSender.java
@@ -1,17 +1,30 @@
 package com.example.surveyapi.domain.share.infra.notification.sender;
 
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Component;
 
 import com.example.surveyapi.domain.share.domain.notification.entity.Notification;
+import com.example.surveyapi.domain.share.infra.annotation.ShareEvent;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Component("EMAIL")
+@RequiredArgsConstructor
 public class NotificationEmailSender implements NotificationSender {
+	private final JavaMailSender mailSender;
+
 	@Override
 	public void send(Notification notification) {
 		log.info("이메일 전송: {}", notification.getId());
-		// TODO : 이메일 실제 전송
+
+		SimpleMailMessage message = new SimpleMailMessage();
+		message.setTo(notification.getRecipientEmail());
+		message.setSubject("공유 알림");
+		message.setText(notification.getShare().getLink());
+
+		mailSender.send(message);
 	}
 }
