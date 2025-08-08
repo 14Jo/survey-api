e64e280f2f4d599c859b7efd33ecae38a1a6745f
taeung515 <xodndd515@gmail.com>
2025-08-08 15:42:30 +0900

    refactor : schedule service 분리

--- src/main/java/com/example/surveyapi/domain/project/application/ProjectService.java
+++ src/main/java/com/example/surveyapi/domain/project/application/ProjectService.java
@@ -1,9 +1,5 @@
 package com.example.surveyapi.domain.project.application;
 
-import java.time.LocalDateTime;
-import java.util.List;
-
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -14,7 +10,6 @@ import com.example.surveyapi.domain.project.application.dto.request.UpdateProjec
 import com.example.surveyapi.domain.project.application.dto.request.UpdateProjectStateRequest;
 import com.example.surveyapi.domain.project.application.dto.response.CreateProjectResponse;
 import com.example.surveyapi.domain.project.domain.project.entity.Project;
-import com.example.surveyapi.domain.project.domain.project.enums.ProjectState;
 import com.example.surveyapi.domain.project.domain.project.event.ProjectEventPublisher;
 import com.example.surveyapi.domain.project.domain.project.repository.ProjectRepository;
 import com.example.surveyapi.global.enums.CustomErrorCode;
@@ -126,26 +121,6 @@ public class ProjectService {
 		Project project = findByIdOrElseThrow(projectId);
 		project.removeMember(currentUserId);
 		publishProjectEvents(project);
-	}
-
-	@Scheduled(cron = "0 0 0 * * *") // 매일 00시 실행
-	@Transactional
-	public void updateProjectStates() {
-		LocalDateTime now = LocalDateTime.now();
-		updatePendingProjects(now);
-		updateInProgressProjects(now);
-	}
-
-	private void updatePendingProjects(LocalDateTime now) {
-		List<Project> pendingProjects = projectRepository.findPendingProjectsToStart(now);
-		List<Long> projectIds = pendingProjects.stream().map(Project::getId).toList();
-		projectRepository.updateStateByIds(projectIds, ProjectState.IN_PROGRESS);
-	}
-
-	private void updateInProgressProjects(LocalDateTime now) {
-		List<Project> inProgressProjects = projectRepository.findInProgressProjectsToClose(now);
-		List<Long> projectIds = inProgressProjects.stream().map(Project::getId).toList();
-		projectRepository.updateStateByIds(projectIds, ProjectState.CLOSED);
 	}
 
 	private void validateDuplicateName(String name) {
