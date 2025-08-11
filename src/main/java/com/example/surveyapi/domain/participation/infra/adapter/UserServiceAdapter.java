b9db2fa26febe5928b1d905dbaecebdfccf4580b
Jang Gyeonghyuk <kcc5107@naver.com>
2025-08-11 10:45:54 +0900

    feat : surveyDetail, participantInfo 캐싱 설정

--- src/main/java/com/example/surveyapi/domain/participation/infra/adapter/UserServiceAdapter.java
+++ src/main/java/com/example/surveyapi/domain/participation/infra/adapter/UserServiceAdapter.java
@@ -1,5 +1,6 @@
 package com.example.surveyapi.domain.participation.infra.adapter;
 
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Component;
 
 import com.example.surveyapi.domain.participation.application.client.UserServicePort;
@@ -18,6 +19,7 @@ public class UserServiceAdapter implements UserServicePort {
 	private final UserApiClient userApiClient;
 	private final ObjectMapper objectMapper;
 
+	@Cacheable(value = "userDetails", key = "#userId", sync = true)
 	@Override
 	public UserSnapshotDto getParticipantInfo(String authHeader, Long userId) {
 		ExternalApiResponse userSnapshot = userApiClient.getParticipantInfo(authHeader, userId);
