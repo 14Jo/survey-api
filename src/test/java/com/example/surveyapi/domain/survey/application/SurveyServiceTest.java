4fbb63b6aa3bd5d05ead55bb7408abdcd061d2da
이준영 <tera199810@gmail.com>
2025-08-12 14:15:28 +0900

    test : 테스트 코드 수정

--- src/test/java/com/example/surveyapi/domain/survey/application/SurveyServiceTest.java
+++ src/test/java/com/example/surveyapi/domain/survey/application/SurveyServiceTest.java
@@ -15,10 +15,10 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.util.ReflectionTestUtils;
 import org.springframework.transaction.annotation.Transactional;
 import org.testcontainers.containers.MongoDBContainer;
@@ -70,11 +70,8 @@ class SurveyServiceTest {
     @Autowired
     private SurveyRepository surveyRepository;
 
-    @MockBean
+    @MockitoBean
     private ProjectPort projectPort;
-
-    @MockBean
-    private SurveyReadSyncService surveyReadSyncService;
 
     private CreateSurveyRequest createRequest;
     private UpdateSurveyRequest updateRequest;
