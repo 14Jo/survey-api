8379e6b1e699075b6916a436f2443dacf7334c2e
이준영 <tera199810@gmail.com>
2025-08-12 10:33:33 +0900

    feat : RabbitMQ설정
    RabbitMQ 설정 - 배치처리, 타임아웃 처리
    이벤트 발행 구조 변경

---
+++ src/main/java/com/example/surveyapi/global/config/RabbitMQConfig.java
@@ -0,0 +1,88 @@
+package com.example.surveyapi.global.config;
+
+import static org.springframework.amqp.core.AcknowledgeMode.*;
+
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.core.Queue;
+
+import com.example.surveyapi.global.constant.RabbitConst;
+
+import lombok.RequiredArgsConstructor;
+
+@Configuration
+@RequiredArgsConstructor
+public class RabbitMQConfig {
+
+	private final ConnectionFactory connectionFactory;
+
+	@Bean
+	public TopicExchange exchange() {
+		return new TopicExchange(RabbitConst.EXCHANGE_NAME);
+	}
+
+	@Bean
+	public Queue queue() {
+		return new Queue(RabbitConst.QUEUE_NAME, true);
+	}
+
+	@Bean
+	public Binding binding(Queue queue, TopicExchange exchange) {
+
+		return BindingBuilder
+			.bind(queue)
+			.to(exchange)
+			.with(RabbitConst.ROUTING_KEY);
+	}
+
+	@Bean
+	public SimpleRabbitListenerContainerFactory batchListenerContainerFactory() {
+
+		SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
+		factory.setConnectionFactory(connectionFactory);
+
+		factory.setConsumerBatchEnabled(true);
+		factory.setBatchSize(100);
+		factory.setBatchReceiveTimeout(5000L);
+
+		factory.setAcknowledgeMode(MANUAL);
+
+		factory.setConcurrentConsumers(1);
+		factory.setMaxConcurrentConsumers(1);
+
+		factory.setMessageConverter(jsonMessageConverter());
+
+		factory.setBatchListener(true);
+
+		return factory;
+	}
+
+	@Bean
+	public SimpleRabbitListenerContainerFactory defaultListenerContainerFactory() {
+
+		SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
+		factory.setConnectionFactory(connectionFactory);
+
+		factory.setConsumerBatchEnabled(false);
+		factory.setMessageConverter(jsonMessageConverter());
+
+		// 동시 처리 설정
+		factory.setConcurrentConsumers(3);
+		factory.setMaxConcurrentConsumers(5);
+
+		return factory;
+	}
+
+	// 🔄 JSON 메시지 변환기
+	@Bean
+	public MessageConverter jsonMessageConverter() {
+		return new Jackson2JsonMessageConverter();
+	}
+}
