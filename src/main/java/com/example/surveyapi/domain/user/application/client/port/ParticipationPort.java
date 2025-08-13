61739906be67d694b014af604bafcbb3e0eb6733
DongGeun <kk506kk@naver.com>
2025-08-13 11:00:31 +0900

    refactor : 패키지 이동 feat : API Timeout 설정

--- src/main/java/com/example/surveyapi/domain/user/application/client/port/ParticipationPort.java
+++ src/main/java/com/example/surveyapi/domain/user/application/client/port/ParticipationPort.java
@@ -1,6 +1,8 @@
-package com.example.surveyapi.domain.user.application.client;
+package com.example.surveyapi.domain.user.application.client.port;
 
 import java.util.List;
+
+import com.example.surveyapi.domain.user.application.client.response.UserSurveyStatusResponse;
 
 public interface ParticipationPort {
     List<UserSurveyStatusResponse> getParticipationSurveyStatus(
