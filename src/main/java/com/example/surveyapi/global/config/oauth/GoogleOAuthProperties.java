f2ce5d1eaff4ce929d81e16c332476457729774d
DongGeun <kk506kk@naver.com>
2025-08-13 10:57:47 +0900

    feat : OAuth 기능 추가 및 추상화,

---
+++ src/main/java/com/example/surveyapi/global/config/oauth/GoogleOAuthProperties.java
@@ -0,0 +1,18 @@
+package com.example.surveyapi.global.config.oauth;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@Component
+@ConfigurationProperties(prefix = "oauth.google")
+public class GoogleOAuthProperties {
+
+    private String clientId;
+    private String clientSecret;
+    private String redirectUri;
+}
