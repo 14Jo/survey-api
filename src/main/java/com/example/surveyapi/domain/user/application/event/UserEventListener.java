fc14f62fda0bb8d62872558e0aa94295af992e31
DongGeun <kk506kk@naver.com>
2025-08-14 23:47:10 +0900

    refactor : 도메인 이벤트 객체를 글러벌 객체로 변경하여 이벤트 발행

--- src/main/java/com/example/surveyapi/domain/user/application/event/UserEventListener.java
+++ src/main/java/com/example/surveyapi/domain/user/application/event/UserEventListener.java
@@ -1,39 +1,26 @@
 package com.example.surveyapi.domain.user.application.event;
 
-import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.event.TransactionPhase;
+import org.springframework.transaction.event.TransactionalEventListener;
 
-import com.example.surveyapi.domain.survey.domain.survey.enums.SurveyStatus;
-import com.example.surveyapi.domain.user.domain.user.User;
-import com.example.surveyapi.domain.user.domain.user.UserRepository;
-import com.example.surveyapi.global.enums.CustomErrorCode;
-import com.example.surveyapi.global.event.SurveyActivateEvent;
-import com.example.surveyapi.global.exception.CustomException;
+import com.example.surveyapi.domain.user.domain.user.event.UserEvent;
+import com.example.surveyapi.global.enums.EventCode;
+import com.example.surveyapi.global.event.UserWithdrawEvent;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 
-@Slf4j
 @Component
 @RequiredArgsConstructor
 public class UserEventListener {
 
-    private final UserRepository userRepository;
+    private final UserEventPublisherPort rabbitPublisher;
+    private final ObjectMapper objectMapper;
 
-    @EventListener
-    public void handlePointIncrease(SurveyActivateEvent event){
-        try{
-            log.info("설문 종료 Id - {} : ", event.getSurveyId());
-
-            if(!event.getSurveyStatus().equals(SurveyStatus.CLOSED)){
-                return;
-            }
-            User user = userRepository.findByIdAndIsDeletedFalse(event.getCreatorID())
-                .orElseThrow(() -> new CustomException(CustomErrorCode.USER_NOT_FOUND));
-
-            user.increasePoint();
-        }catch (Exception e){
-            log.error("포인트 상승 실패 , 등급 상승 실패 : {}", e.getMessage());
-        }
+    @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
+    public void handle(UserEvent domainEvent){
+        UserWithdrawEvent globalEvent = objectMapper.convertValue(domainEvent, UserWithdrawEvent.class);
+        rabbitPublisher.publish(globalEvent, EventCode.USER_WITHDRAW);
     }
 }
