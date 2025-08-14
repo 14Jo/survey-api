223c56613a3c4957741650488e054fe40defb7de
easter1201 <kite1201kr@gmail.com>
2025-08-14 16:57:31 +0900

    feat : 토큰 저장 위한 api 추가

---
+++ src/main/java/com/example/surveyapi/domain/share/api/external/FcmController.java
@@ -0,0 +1,31 @@
+package com.example.surveyapi.domain.share.api.external;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.surveyapi.domain.share.application.fcm.FcmTokenService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v2/fcm")
+public class FcmController {
+	private final FcmTokenService tokenService;
+
+	@PostMapping("/token")
+	public ResponseEntity<Void> save(
+		@RequestParam String token,
+		@AuthenticationPrincipal Long userId
+	) {
+		tokenService.saveToken(userId, token);
+
+		return ResponseEntity.status(HttpStatus.OK)
+			.build();
+	}
+}
