b63ad30e7abd29b7b5aa96eabad04069345de7b9
이준영 <tera199810@gmail.com>
2025-08-14 11:50:21 +0900

    refactor : 이벤트 발행 아키텍처 개편

--- src/main/java/com/example/surveyapi/domain/survey/infra/event/RabbitPublisherImpl.java
+++ src/main/java/com/example/surveyapi/domain/survey/infra/event/RabbitPublisherImpl.java
@@ -3,6 +3,7 @@ package com.example.surveyapi.domain.survey.infra.event;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Service;
 
+import com.example.surveyapi.domain.survey.application.event.RabbitPublisherPort;
 import com.example.surveyapi.global.constant.RabbitConst;
 import com.example.surveyapi.global.enums.EventCode;
 import com.example.surveyapi.global.model.SurveyEvent;
@@ -11,11 +12,11 @@ import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
-public class EventPublisher {
+public class RabbitPublisherImpl implements RabbitPublisherPort {
 
 	private final RabbitTemplate rabbitTemplate;
 
-	public void publishEvent(SurveyEvent event, EventCode key) {
+	public void publish(SurveyEvent event, EventCode key) {
 		String routingKey = RabbitConst.ROUTING_KEY.replace("#", key.name());
 		rabbitTemplate.convertAndSend(RabbitConst.EXCHANGE_NAME, routingKey, event);
 	}
