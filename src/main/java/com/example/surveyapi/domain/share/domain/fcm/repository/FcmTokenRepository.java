7caeff5e1805f5a193bae382534221646543c2d7
easter1201 <kite1201kr@gmail.com>
2025-08-14 16:56:53 +0900

    feat : 토큰 repository 추가

---
+++ src/main/java/com/example/surveyapi/domain/share/domain/fcm/repository/FcmTokenRepository.java
@@ -0,0 +1,13 @@
+package com.example.surveyapi.domain.share.domain.fcm.repository;
+
+import java.util.Optional;
+
+import org.springframework.stereotype.Repository;
+
+import com.example.surveyapi.domain.share.domain.fcm.entity.FcmToken;
+
+@Repository
+public interface FcmTokenRepository {
+	FcmToken save(FcmToken token);
+	Optional<FcmToken> findByUserId(Long userId);
+}
