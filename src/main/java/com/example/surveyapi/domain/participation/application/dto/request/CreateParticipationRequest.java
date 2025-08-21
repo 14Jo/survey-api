832991f24a6eac6862bd642c46f657f4a93eb60d
Jang Gyeonghyuk <kcc5107@naver.com>
2025-08-21 13:12:50 +0900

    feat : 유저 정보 스냅샷 클라이언트로부터 받는 것으로 변경, 설문 응답 제출 RequestBody 수정

--- src/main/java/com/example/surveyapi/domain/participation/application/dto/request/CreateParticipationRequest.java
+++ src/main/java/com/example/surveyapi/domain/participation/application/dto/request/CreateParticipationRequest.java
@@ -3,8 +3,11 @@ package com.example.surveyapi.domain.participation.application.dto.request;
 import java.util.List;
 
 import com.example.surveyapi.domain.participation.domain.command.ResponseData;
+import com.example.surveyapi.domain.participation.domain.participation.enums.Gender;
+import com.example.surveyapi.domain.participation.domain.participation.vo.Region;
 
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 
 @Getter
@@ -12,5 +15,14 @@ public class CreateParticipationRequest {
 
 	@NotEmpty(message = "응답 데이터는 최소 1개 이상이어야 합니다.")
 	private List<ResponseData> responseDataList;
+
+	@NotNull(message = "사용자 생년월일은 필수입니다.")
+	private String birth;
+
+	@NotNull(message = "사용자 성별은 필수입니다.")
+	private Gender gender;
+
+	@NotNull(message = "사용자 지역 정보는 필수입니다.")
+	private Region region;
 }
 
