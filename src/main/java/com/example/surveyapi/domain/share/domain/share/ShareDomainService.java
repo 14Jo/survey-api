da88e803d72b10d27700a49d5a13d46379ac0ed1
easter1201 <kite1201kr@gmail.com>
2025-08-25 11:36:27 +0900

    feat : 링크 수정

--- src/main/java/com/example/surveyapi/domain/share/domain/share/ShareDomainService.java
+++ src/main/java/com/example/surveyapi/domain/share/domain/share/ShareDomainService.java
@@ -12,9 +12,9 @@ import com.example.surveyapi.global.exception.CustomException;
 
 @Service
 public class ShareDomainService {
-	private static final String SURVEY_URL = "http://localhost:8080/share/surveys/";
-	private static final String PROJECT_MEMBER_URL = "http://localhost:8080/share/projects/members/";
-	private static final String PROJECT_MANAGER_URL = "http://localhost:8080/share/projects/managers/";
+	private static final String SURVEY_URL = "https://localhost:8080/share/surveys/";
+	private static final String PROJECT_MEMBER_URL = "https://localhost:8080/share/projects/members/";
+	private static final String PROJECT_MANAGER_URL = "https://localhost:8080/share/projects/managers/";
 
 	public Share createShare(ShareSourceType sourceType, Long sourceId,
 		Long creatorId,	LocalDateTime expirationDate) {
@@ -44,7 +44,7 @@ public class ShareDomainService {
 		} else if(share.getSourceType() == ShareSourceType.PROJECT_MANAGER) {
 			return "https://localhost:8080/api/projects/managers/" + share.getSourceId();
 		} else if (share.getSourceType() == ShareSourceType.SURVEY) {
-			return "http://localhost:8080/surveys/" + share.getSourceId();
+			return "https://localhost:8080/surveys/" + share.getSourceId();
 		}
 		throw new CustomException(CustomErrorCode.INVALID_SHARE_TYPE);
 	}
